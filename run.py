84aba55a45514173e867158522f45618c56c0d37
seanj06 <seanj06@hotmail.com>
2022-11-11 21:46:37 +0000

    Add function to create new deck and unit test function

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def new_deck():
+
+    suits = {'\u2660', '\u2661', '\u2662', '\u2663'}
+    ranks = {'2','3','4','5','6','7','8','9','10','J','Q','K','A'}
+    deck = []
+
+    for suit in suits:
+        for rank in ranks:
+            deck.append(rank + suit)
+
+    return deck
+
+mydeck = new_deck()
+print(mydeck)            
