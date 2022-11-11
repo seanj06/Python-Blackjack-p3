e7662f0f017d68b28f73155312f56cb1a1a02a7d
seanj06 <seanj06@hotmail.com>
2022-11-11 21:52:35 +0000

    Change dictionary variables to lists and add shuffle method to deck

--- run.py
+++ run.py
@@ -2,15 +2,17 @@ import random
 
 def new_deck():
 
-    suits = {'\u2660', '\u2661', '\u2662', '\u2663'}
-    ranks = {'2','3','4','5','6','7','8','9','10','J','Q','K','A'}
+    suits = ['\u2660', '\u2661', '\u2662', '\u2663']
+    ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
     deck = []
 
     for suit in suits:
         for rank in ranks:
             deck.append(rank + suit)
 
+    random.shuffle(deck)
+
     return deck
 
 mydeck = new_deck()
-print(mydeck)            
+print(mydeck)       
