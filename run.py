433b1224a6adc28dabe0cceede13fdaca6f39a48
seanj06 <seanj06@hotmail.com>
2022-11-25 03:44:42 +0000

    Remove all whitespace for pylint validation

--- run.py
+++ run.py
@@ -59,7 +59,7 @@ def hand_value(hand):
 
     while total > 21 and aces > 0:
         total -= 10
-        aces -= 1 
+        aces -= 1
 
     return total
 
@@ -67,7 +67,7 @@ def hand_value(hand):
 def check_winner(dealer, player, chips):
     """
     Compares total hand values of dealer and player and determines winner
-    """ 
+    """
     if hand_value(player) == 21 and hand_value(dealer) != 21:
         print("\nYou win with a blackjack")
         player_chips.win_bet()
@@ -75,18 +75,18 @@ def check_winner(dealer, player, chips):
         print("\nDealer wins with a blackjack")
         player_chips.lose_bet()
     elif hand_value(player) > 21:
-        print("\nSorry you have busted") 
+        print("\nSorry you have busted")
         player_chips.lose_bet()
     elif hand_value(dealer) > 21 and hand_value(player) < 22:
-        print("\nDealer busts you win") 
-        player_chips.win_bet()       
+        print("\nDealer busts you win")
+        player_chips.win_bet()
     elif hand_value(player) == hand_value(dealer):
-        print(f"\nYou both have {hand_value(player)} its a draw") 
+        print(f"\nYou both have {hand_value(player)} its a draw")
     elif 21 - hand_value(player) < 21 - hand_value(dealer):
-        print("\nYou win") 
+        print("\nYou win")
         player_chips.win_bet()
     elif 21 - hand_value(dealer) < 21 - hand_value(player):
-        print("\nDealer wins")  
+        print("\nDealer wins")
         player_chips.lose_bet()
 
 
@@ -95,19 +95,19 @@ def main():
     Main game loop
     """
     playing = True
-    while playing:       
+    while playing:
         player_go = True
         deck = new_deck()
         player = []
-        dealer = [] 
+        dealer = []
         print(f"You have {player_chips.chip_balance} chips ")
         bet(player_chips)
         for i in range(2):
             deal_card(deck, player)
             deal_card(deck, dealer)
         print(f"\nYour cards are {' '.join(player)}\
-  Total: {(hand_value(player))}\n") 
-        print(f"Dealers cards are {dealer[0]}, x\n") 
+  Total: {(hand_value(player))}\n")
+        print(f"Dealers cards are {dealer[0]}, x\n")
         while player_go is True and hand_value(player) < 21:
             try:
                 hit_or_stand = input("Would you like to [H]it or [S]tand: ")
@@ -117,23 +117,23 @@ def main():
   Total: {(hand_value(player))}")
                 elif hit_or_stand.lower() == "s":
                     print(f"Dealer shows {' '.join(dealer)}\
-  Total: {(hand_value(dealer))}") 
+  Total: {(hand_value(dealer))}")
                     player_go = False
             except ValueError:
-                print("Invalid input please press H to hit or S to stand")         
+                print("Invalid input please press H to hit or S to stand")
         while hand_value(dealer) < 17 and hand_value(player) < 22:
             deal_card(deck, dealer)
             print(f"The dealer has {' '.join(dealer)}\
   Total: {(hand_value(dealer))}\n")
 
-        check_winner(dealer, player, Chips) 
+        check_winner(dealer, player, Chips)
         print(f"\nYou have {player_chips.chip_balance} chips")
         if player_chips.chip_balance == 0:
             print("Sorry you have run out of chips, game over!")
             playing = False
             player_chips.chip_balance = 500
         play_again()
-        
+
 
 def start_screen():
     """
@@ -148,12 +148,12 @@ def start_screen():
             if start_game.lower() == 's':
                 name_input()
             elif start_game.lower() == 'i':
-                instructions()       
+                instructions()
         except ValueError:
-            print("Invalid input") 
+            print("Invalid input")
         else:
-            print("Invalid input")    
-        
+            print("Invalid input")
+
 
 def play_again():
     """
@@ -168,9 +168,9 @@ def play_again():
                 print("\nThanks for playing Goodbye")
                 player_chips.chip_balance = 500
                 start_screen()
-        except ValueError:  
+        except ValueError:
             print("Invalid input")
-            continue  
+            continue
         else:
             print("Invalid input please type Y for yes or N for no")
 
@@ -186,7 +186,7 @@ def name_input():
                 type_text(f"\nWelcome to the game {player_name} ")
                 main()
         except ValueError:
-            print("Invalid input") 
+            print("Invalid input")
         else:
             print("Invalid input please enter your name")
 
@@ -194,12 +194,12 @@ def name_input():
 def instructions():
     """
     Function to show player game instructions
-    """ 
+    """
     type_text("Welcome to blackjack the card game of skill and luck.\n\n "
               "The aim of the game is to get to as close to 21 as you can "
               "without going over it.\n\n "
               "If you do you will bust!\n\n "
-              "Each card number is worth that value, Jacks, Queens " 
+              "Each card number is worth that value, Jacks, Queens "
               "and Kings\n "
               "are worth 10 and Aces are worth both 1 and 11.\n\n "
               "You will be dealt 2 cards at the start of the game and will be "
@@ -209,19 +209,19 @@ def instructions():
               "The dealer will also be dealt 2 cards but you will only be "
               "shown 1 until you have completed your turn.\n\n "
               "The dealer has to take another card if they have "
-              "less than 17.\n\n") 
+              "less than 17.\n\n")
     main_menu = input("Press any key to return to the main menu")
     try:
         if "" in main_menu:
             start_screen()
     except ValueError:
-        print("Invalid input")              
+        print("Invalid input")
 
 
 class Chips:
     """
     Gives users chips to play game and adds and deducts chips per bet
-    """ 
+    """
 
     def __init__(self, chip_balance=500):
         self.chip_balance = chip_balance
@@ -237,7 +237,7 @@ class Chips:
         """
         If bet lost deducts from current balance
         """
-        self.chip_balance -= self.current_bet                 
+        self.chip_balance -= self.current_bet
 
 
 def bet(chips):
@@ -255,19 +255,8 @@ def bet(chips):
             elif chips.current_bet < 1:
                 print("Please type a valid bet amount")
             else:
-                break    
-    
+                break
 
-player_chips = Chips()            
+
+player_chips = Chips()
 start_screen()
-
-
-
-
-
-
-
-
-
-
-
